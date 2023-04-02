991cd10d454cedce496230b7c1bd1cbb8226de6c
rhsabbir1 <rhsabbir090@gmai.com>
2023-04-02 17:09:06 +0600

    menu responsive done

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = ({route}) => {
     return (
-        <li>
+        <li className='m-4 hover:bg-orange-400 px-4'>
             {route.name}
         </li>
     );
--- src/components/Navber/Navber.jsx
+++ src/components/Navber/Navber.jsx
@@ -1,49 +1,55 @@
 import React, { useState } from 'react';
 import Link from '../Link/Link';
-import { Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navber = () => {
-    const [open , setOpen] = useState(false)
+    const [open, setOpen] = useState(false)
     const routes = [
         {
-          id: 1,
-          name: 'Home',
-          path: '/'
+            id: 1,
+            name: 'Home',
+            path: '/'
         },
         {
-          id: 2,
-          name: 'About',
-          path: '/about'
+            id: 2,
+            name: 'About',
+            path: '/about'
         },
         {
-          id: 3,
-          name: 'Contact',
-          path: '/contact'
+            id: 3,
+            name: 'Contact',
+            path: '/contact'
         },
         {
-          id: 4,
-          name: 'Products',
-          path: '/products'
+            id: 4,
+            name: 'Products',
+            path: '/products'
         },
         {
-          id: 5,
-          name: 'Services',
-          path: '/services'
+            id: 5,
+            name: 'Services',
+            path: '/services'
         }
-      ];
-      
+    ];
+
 
     return (
         <nav>
-            <div onClick={()=> setOpen(!open)}>
-                <span>{open === true ? "Open" : "close"}</span>
-                 <Bars3Icon className="h-8 w-8 text-yellow-500" />
+            <div onClick={() => setOpen(!open)} className='md:hidden'>
+                <span>{
+                    open === true ?
+                        <XMarkIcon className="h-8 w-8 text-yellow-500" />
+                        :
+                        <Bars3Icon className="h-8 w-8 text-yellow-500" />
+                }
+                </span>
+                
             </div>
-            <ul>
+            <ul className={`bg-slate-300 duration-500 md:flex absolute md:static ${open ? "top-6" : '-top-64'}`}>
                 {
-                    routes.map(route =><Link
-                    key={route.id}
-                    route={route}
+                    routes.map(route => <Link
+                        key={route.id}
+                        route={route}
                     ></Link>)
                 }
             </ul>
