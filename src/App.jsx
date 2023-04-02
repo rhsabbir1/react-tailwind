1a6979938c7abc0ead1b38ff804cb9d061bc2b6f
rhsabbir1 <rhsabbir090@gmai.com>
2023-04-02 16:18:13 +0600

    use tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navber from './components/Navber/Navber'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Navber></Navber>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
---
+++ src/components/Navber/Navber.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Navber = () => {
+
+    const routes = [
+        {
+          id: 1,
+          name: 'Home',
+          path: '/'
+        },
+        {
+          id: 2,
+          name: 'About',
+          path: '/about'
+        },
+        {
+          id: 3,
+          name: 'Contact',
+          path: '/contact'
+        },
+        {
+          id: 4,
+          name: 'Products',
+          path: '/products'
+        },
+        {
+          id: 5,
+          name: 'Services',
+          path: '/services'
+        }
+      ];
+      
+
+    return (
+        <nav>
+            <ul>
+                {
+                    routes.map(route =><li>{route.name}</li>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navber;
